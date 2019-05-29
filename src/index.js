34af53a771ce92633799f0b080f7cdc520d2843e
1Cbondy1 <claytonbondy@gmail.com>
2019-05-28 19:31:29 -0500

    added unordered list

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<ul><li>one</li><li>two</li><li>three</li></ul>, document.getElementById('root'));
 
 serviceWorker.unregister();
